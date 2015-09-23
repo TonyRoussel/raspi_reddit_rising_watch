9da7f5a505faaddd1bb894bdb74d23c3eebf3269
shortyp <tonydraf@gmail.com>
2015-09-24 00:48:36 +0200

    add gpio support

--- reddit_rising_watch_0.0.1.py
+++ reddit_rising_watch_0.0.1.py
@@ -2,29 +2,61 @@
 import praw
 import sys
 import time
+import RPi.GPIO as GPIO
 
 
-def cooldown():
-    print "cooldown on"
-    time.sleep(2)
-    print "cooldown off"
-    return
+gpio_new_rise = 7
+gpio_retrive = 11
 
-
-def notify(count):
-    print count
-    return
+gpio_list = [gpio_new_rise, gpio_retrieve]
 
 
 rising_retrieve_limit = 10
+cooldown_time = 2
+
+
+def init_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    for gpio in gpio_list:
+        GPIO.setup(gpio, GPIO.OUT)
+    return
+
+
+def gpio_blink(pin, bton=1.):
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(bton)
+    GPIO.output(pin, GPIO.LOW)
+    return
+
+
+def close_gpio():
+    for gpio in gpio_list:
+        GPIO.output(gpio, GPIO.LOW)
+    GPIO.cleanup()
+    return
+
+
+def cooldown(pin):
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(cooldown_time)
+    return
+
+
+def notify(pin, count):
+    for i in xrange(count):
+        gpio_blink(pin, bton=1/4.)
+        time.sleep(1/3.)
+    return
 
 
 try:
+    init_gpio()
     user_agent = "linux:Rising Watch:v0.0.1 (by /u/not_da_bot)"
     r = praw.Reddit(user_agent=user_agent)
     last_ids = []
     r.login()
     while 1:
+        GPIO.output(gpio_retrieve, GPIO.HIGH)
         new_count = 0
         ids_list = []
         rising = r.get_rising(limit=rising_retrieve_limit)
@@ -34,8 +66,8 @@ try:
             ids_list.append(submission.id)
         last_ids = list(ids_list)
         if new_count != 0:
-            notify(new_count)
-        cooldown()
+            notify(gpio_new_rise, new_count)
+        cooldown(gpio_retrieve)
 except KeyboardInterrupt:
     print >> sys.stderr, "\nKeyboard Interruption\n"
 except praw.errors.InvalidUserPass:
@@ -43,4 +75,5 @@ except praw.errors.InvalidUserPass:
 except:
     print >> sys.stderr, "\nUnexpected Exception\n"
 finally:
+    close_gpio()
     print >> sys.stderr, "Bye"
