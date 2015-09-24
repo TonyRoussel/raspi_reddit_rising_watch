4454711ecc695fbe850be4561614cc4f8f7d93ef
shortyp <tonydraf@gmail.com>
2015-09-24 04:17:20 +0200

    extend LED display, reorganise notify

--- reddit_rising_watch_0.0.1.py
+++ reddit_rising_watch_0.0.1.py
@@ -5,10 +5,14 @@ import time
 import RPi.GPIO as GPIO
 
 
-gpio_new_rise = 7
-gpio_retrieve = 11
+gpio_retrieve = 7
+gpio_new_rise = 11
+gpio_comm_num = 13
+gpio_nsfw = 15
 
-gpio_list = [gpio_new_rise, gpio_retrieve]
+gpio_list = [gpio_new_rise, gpio_retrieve, gpio_comm_num, gpio_nsfw]
+
+time_new_rise = 1/4.
 
 
 rising_retrieve_limit = 10
@@ -22,10 +26,12 @@ def init_gpio():
     return
 
 
-def gpio_blink(pin, bton=1.):
+def gpio_blink(pin, bton=1., btoff=None):
     GPIO.output(pin, GPIO.HIGH)
     time.sleep(bton)
     GPIO.output(pin, GPIO.LOW)
+    if btoff is not None:
+        time.sleep(btoff)
     return
 
 
@@ -42,7 +48,7 @@ def cooldown(pin):
     return
 
 
-def notify(pin, submissions):
+def notify(submissions):
     for submission in submissions:
         title = submission.title.encode('utf-8')
         subreddit_name = submission.subreddit.display_name.encode('utf-8')
@@ -52,8 +58,14 @@ def notify(pin, submissions):
             print "- %s [/r/%s] (%d comments)" % (title, subreddit_name, comments_count)
         else:
             print "- %s [/r/%s] (%d comments) {NSFW}" % (title, subreddit_name, comments_count)
-        gpio_blink(pin, bton=1/4.)
-        time.sleep(1/4.)
+        if comments_count != 0:
+            time_comm_num = time_new_rise / float(comments_count))
+            GPIO.output((gpio_new_rise, gpio_nsfw), (GPIO.HIGH, GPIO.HIGH if nsfw else GPIO.LOW))
+            for i in xrange(comments_count):
+                gpio_blink(gpio_comm_num, bton=time_comm_num/2., btoff=time_comm_num/2.)
+            GPIO.output((gpio_new_rise, gpio_nsfw), (GPIO.LOW, GPIO.LOW))
+        else:
+            gpio_blink(gpio_new_rise, bton=time_new_rise)
     print ""
     return
 
@@ -75,7 +87,7 @@ try:
             ids_list.append(submission.id)
         last_ids = list(ids_list)
         if len(new_submissions) != 0:
-            notify(gpio_new_rise, new_submissions)
+            notify(new_submissions)
         cooldown(gpio_retrieve)
 except KeyboardInterrupt:
     print >> sys.stderr, "\nKeyboard Interruption\n"
