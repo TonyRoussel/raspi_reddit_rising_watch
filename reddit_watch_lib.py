a10d5fe3fb742fae00e3bd599e7da5f6382cbdee
shortyp <tonydraf@gmail.com>
2015-10-01 01:55:21 +0200

    add key press detection function

--- reddit_watch_lib.py
+++ reddit_watch_lib.py
@@ -1,4 +1,8 @@
 import argparse
+import thread
+import sys
+import tty
+import termios
 
 
 def check_positive_float(value):
@@ -13,3 +17,24 @@ def list_unique_merge(l1, l2):
         if x not in l1:
             l1.append(x)
     return
+
+
+def key_detect():
+    char = [None]
+    thread.start_new_thread(_keydetect_init, (char,))
+    return char
+
+
+def _keydetect_init(char):
+    char[0] = _keydetect_core()
+
+
+def _keydetect_core():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
